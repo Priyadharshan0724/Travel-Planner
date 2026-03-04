2508cf530d54e0d606e30752b79d37bd435663d6
Priyadharshan0724 <priyadharshan0724@gmail.com>
2026-03-04 21:20:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+				
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function (elem) {
+  for (let i = 0; i < navElemArr.length; i++) {
+    navElemArr[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+  }
+};
+
+/**
+ * header & go top btn active on scroll
+ */
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+}); 
+ (navElemArr.length > 0) ? navToggleEvent() : null;
+(navElemArr.length > 0) ? navToggleEvent() : null;
+for (let i = 0; i < navElemArr.length; i++) {
+    navElemArr[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+    }
+navLinks.forEach(function (navLink) {
+    navLink.addEventListener("click", function () {
+        navbar.classList.remove("active");
+        overlay.classList.remove("active");
+    }
+    );
+});
